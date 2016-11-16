f241e386534a8340a22f4e9ccc8771112d5dbaf3
kriizjones <kristoffer.s.karlsson@hotmail.com>
2016-11-16 12:11:23 +0100

    modified withdraw method to take account object

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,20 @@
 class Atm
-attr_accessor :funds
+  attr_accessor :funds
 
-def initialize
-  @funds = 1000
-end
+  def initialize
+    @funds = 1000
+  end
+
+  def withdraw(amount, account)
+    case
+    when amount > account.balance then
+      return
+    else
+      @funds -= amount
+      account.balance -= amount
+      { status: true, message: 'success', date: Date.today, amount: amount}
+
+    end
+    #@funds -= amount
+  end
 end
