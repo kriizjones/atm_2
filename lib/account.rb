69ff8ca11260ab4e0e98e1d3f791e55ad354b2fb
kriizjones <kristoffer.s.karlsson@hotmail.com>
2016-11-17 12:48:06 +0100

    account going active

---
+++ lib/account.rb
@@ -0,0 +1,14 @@
+class Account
+  attr_accessor :pin_code, :balance
+
+  def initialize
+    @pin_code = generate_pin
+    @balance = 0
+  end
+
+  private
+
+  def generate_pin
+    rand(1000..9999)
+  end
+end
